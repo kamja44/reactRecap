886d3eb92542ab5d12add900f7fa95e736bedb8a
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-01-20 11:26:57 +0900

    7.0 ~ 7.1
    Create ToDo List

--- react-recap/src/App.js
+++ react-recap/src/App.js
@@ -1,6 +1,36 @@
 import { useState, useEffect } from "react";
 function App() {
-  return <div></div>;
+  const [toDo, setToDo] = useState("");
+  const [toDoArray, setToDoArray] = useState([]);
+  const onChange = (event) => setToDo((data) => event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDoArray((currentArray) => [toDo, ...currentArray]);
+    setToDo((data) => "");
+  };
+  return (
+    <div>
+      <h1>My To Do List ({toDoArray.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDoArray.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
