d785035cffa04d6bbafbc7b660e0c06b58065e9a
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-01-20 10:36:03 +0900

    basic Setting

--- react-recap/src/App.js
+++ react-recap/src/App.js
@@ -1,29 +1,6 @@
 import { useState, useEffect } from "react";
-function Hello() {
-  // function
-  useEffect(function () {
-    console.log("hi");
-    return function () {
-      console.log("bye");
-    };
-  }, []);
-  // arrow Function
-  useEffect(() => {
-    console.log("h1");
-    return () => console.log("bye");
-  }, []);
-  // function 코드와 arrow Function코드는 서로 동일하다.
-  return <h1>Hello</h1>;
-}
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((data) => !data);
-  return (
-    <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Showing"}</button>
-      {showing ? <Hello /> : null}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
